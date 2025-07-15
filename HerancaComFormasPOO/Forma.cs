5271c411842cf5088bebf7360a813c92005b722e
João Gabriel <sospox8@gmail.com>
2025-07-15 15:34:37 -0300

    Usando Herança com Formas Geométrica - POO

---
+++ HerancaComFormasPOO/Forma.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace desafiosPOOAlura.Classes
+{
+    public class Forma
+    {
+        public virtual void CalculaPerimetro()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculando perímetro...");
+            Thread.Sleep(2000);
+        }
+
+        public virtual void CalculaArea()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculando área...");
+            Thread.Sleep(2000);
+        }
+    }
+
+    public class Quadrado : Forma
+    {
+        private double Lado { get; set; }
+
+        public Quadrado(double lado)
+        {
+            Lado = lado;
+        }
+
+        public override void CalculaPerimetro()
+        {
+            // L * 4
+            base.CalculaPerimetro();
+            var perimetro = Lado * 4;
+            Console.WriteLine($"O perímetro do Quadrado de lados {Lado} é de {perimetro}");
+        }
+        public override void CalculaArea()
+        {
+            // L * L
+            base.CalculaArea();
+            var area = Lado * Lado;
+            Console.WriteLine($"A área do Quadrado de lados {Lado} é de {area}");
+        }
+    }
+
+    public class Triangulo : Forma
+    {
+
+        private double Lado { get; set; }
+        private double Base { get; set; }
+        private double Altura { get; set; }
+        public Triangulo(double lado)
+        {
+            Lado = lado;
+        }
+
+        public Triangulo(double baseT, double altura)
+        {
+            Base = baseT;
+            Altura = altura;
+        }
+
+        public override void CalculaPerimetro()
+        {
+            // L * 3
+            base.CalculaPerimetro();
+            var perimetro = Lado * 3;
+            Console.WriteLine($"O perímetro do Triângulo de lados {Lado} é de {perimetro}");
+        }
+        public override void CalculaArea()
+        {
+            // B * A / 2
+            base.CalculaArea();
+            var area = (Base * Altura) / 2;
+            Console.WriteLine($"A área do Triângulo de BASE={Base} e ALTURA={Altura} é de {area}");
+        }
+    }
+
+    public class Circulo : Forma
+    {
+        private double Raio { get; set; }
+        private double pi = Math.PI;
+
+        public Circulo(double raio)
+        {
+            Raio = raio;
+        }
+
+        public override void CalculaPerimetro()
+        {
+            // 2pi * r
+            base.CalculaPerimetro();
+            var perimetro = (2 * pi) * Raio;
+            Console.WriteLine($"O perímetro do Ciculo de RAIO {Raio} é de {perimetro}");
+        }
+        public override void CalculaArea()
+        {
+            // pi * r^2
+            base.CalculaArea();
+            var area = pi * (Raio * Raio);
+            Console.WriteLine($"A área do Circulo de RAIO {Raio} é de {area}");
+        }
+    }
+}
---
+++ HerancaComFormasPOO/Program.cs
@@ -0,0 +1,89 @@
+﻿using desafiosPOOAlura.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoHerancaComFormas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, Forma> dic = new Dictionary<int, Forma>();
+
+            Console.WriteLine("Calculador de Área e Perímetro\n\nEscolha uma das opções\n[1] Quadrado\n[2] Triângulo (Equilatero)\n[3] Círculo");
+            Console.Write("Sua escolha: ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            string choose = " ";
+
+            if (escolha == 1) choose = "Quadrado";
+            if (escolha == 2) choose = "Triangulo";
+            if (escolha == 3) choose = "Circulo";
+
+            Console.Clear();
+            Console.WriteLine($"Digite [1] para calcular a área ou [2] para calcular o perímetro\n");
+            Console.Write("Sua escolha: ");
+            int escolha2 = int.Parse(Console.ReadLine());
+
+            double escolha3;
+
+            //perimetros
+            if (escolha2 == 2 && (escolha == 1 || escolha == 2))
+            {
+                Console.Clear();
+                Console.WriteLine($"Perímetro do {choose}");
+                Console.Write($"Digite os LADOS do {choose}: ");
+                escolha3 = double.Parse(Console.ReadLine());
+                if (escolha == 1) dic.Add(escolha, new Quadrado(escolha3));
+                if (escolha == 2) dic.Add(escolha, new Triangulo(escolha3));
+                dic[escolha].CalculaPerimetro();
+            }
+            if (escolha2 == 2 && escolha == 3)
+            {
+                Console.Clear();
+                Console.WriteLine($"Perímetro do {choose}");
+                Console.Write($"Digite o RAIO do {choose}: ");
+                escolha3 = double.Parse(Console.ReadLine());
+                dic.Add(escolha, new Circulo(escolha3));
+                dic[escolha].CalculaPerimetro();
+            }
+
+            //areas
+            if (escolha2 == 1 && escolha == 1)
+            {
+                Console.Clear();
+                Console.WriteLine($"Área do {choose}");
+                Console.Write($"Digite os LADOS do {choose}: ");
+                escolha3 = double.Parse(Console.ReadLine());
+                dic.Add(escolha, new Quadrado(escolha3));
+                dic[escolha].CalculaArea();
+            }
+            if (escolha2 == 1 && escolha == 2)
+            {
+                Console.Clear();
+                Console.WriteLine($"Área do {choose}");
+                Console.Write($"Digite a BASE do {choose}: ");
+                escolha3 = double.Parse(Console.ReadLine());
+                Console.Write($"Digite a ALTURA do {choose}: ");
+                double alturaTriangulo = double.Parse(Console.ReadLine());
+                dic.Add(escolha, new Triangulo(escolha3, alturaTriangulo));
+                dic[escolha].CalculaArea();
+            }
+            if (escolha2 == 1 && escolha == 3)
+            {
+                Console.Clear();
+                Console.WriteLine($"Área do {choose}");
+                Console.Write($"Digite o RAIO do {choose}: ");
+                escolha3 = double.Parse(Console.ReadLine());
+                dic.Add(escolha, new Circulo(escolha3));
+                dic[escolha].CalculaArea();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
